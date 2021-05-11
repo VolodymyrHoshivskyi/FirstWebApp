2c1b334cea50bac3724ace2da1318d9692f69392
VolodymyrHoshivskyi <49150297+VolodymyrHoshivskyi@users.noreply.github.com>
2021-05-11 23:22:20 +0300

    Add files via upload

---
+++ FirstWebApp/FirstWebApp/Data/Interfaces/IAllCars.cs
@@ -0,0 +1,14 @@
+﻿using FirstWebApp.Data.Models;
+using System.Collections.Generic;
+
+namespace FirstWebApp.Data.Interfaces
+{
+    public interface IAllCars
+    {
+
+        IEnumerable<Car> Cars { get; }
+        IEnumerable<Car> getFavCars { get; set; }
+        Car getObjectCar (int carID);
+
+    }
+}
---
+++ FirstWebApp/FirstWebApp/Data/Interfaces/ICarsCategory.cs
@@ -0,0 +1,14 @@
+﻿using FirstWebApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWebApp.Data.Interfaces
+{
+    public interface ICarsCategory { 
+
+        IEnumerable<Category> AllCategories { get; } // повертає список з типом данних Category
+    
+    }
+}
